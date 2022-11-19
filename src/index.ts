c2a645a84457599b29aeb667c44d295c592d9710
Chris Freeman <cfreeman.code@gmail.com>
2022-11-19 14:25:05 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createSignal, type Signal } from './signal';
 export { createDerived, type Derived } from './derived';
 export { createEffect, type Effect } from './effect';
+export { TrackedSet } from './collections/tracked-set';
