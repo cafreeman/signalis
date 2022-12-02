5ba255ac11da9408ff82af192311264c7af7feaa
Chris Freeman <cfreeman.code@gmail.com>
2022-12-01 20:57:48 -0700

    export batch

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export { createSignal, type Signal } from './signal.js';
 export { createDerived, type Derived } from './derived.js';
 export { createEffect } from './effect.js';
+export { batch } from './batch.js';
 export { Reaction } from './reaction.js';
 export { createResource, type Resource, type ResourceWithSignal } from './resource.js';
 export { TrackedSet } from './collections/tracked-set.js';
