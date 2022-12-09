9fb2f6c253baee87da5af3be53468dcc13ef763c
Chris Krycho <hello@chriskrycho.com>
2022-12-09 15:47:47 -0700

    Eliminate the need for casts in `createSignal()`
    By explicitly distinguishing between non-nullable and nullable creation
    paths in the overloads, and then explicitly checking `== null` in the
    constructor call, we give TS enough info to eliminate the need for the
    casts. This also requires propagating that back to `useSignal()`, which
    is a mild annoyance, but provides *much* better type safe behavior.

--- packages/react/src/useSignal.ts
+++ packages/react/src/useSignal.ts
@@ -1,7 +1,9 @@
-import { createSignal } from '@signalis/core';
+import { createSignal, type Signal } from '@signalis/core';
 import { useMemo } from 'react';
 import { EMPTY } from './empty.js';
 
-export function useSignal<T>(value: T) {
-  return useMemo(() => createSignal(value), EMPTY);
+export function useSignal(value?: null | undefined): Signal<unknown>;
+export function useSignal<T extends {}>(value: T): Signal<T>;
+export function useSignal<T extends {}>(value?: T | null | undefined): Signal<T> | Signal<unknown> {
+  return useMemo(() => (value ? createSignal(value) : createSignal(null)), EMPTY);
 }
