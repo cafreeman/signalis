e84f7b8e6a8624f21332060ecb4edb802fa9d5c6
Chris Freeman <cfreeman.code@gmail.com>
2023-01-02 12:26:47 -0700

    get Resources working again

--- packages/core/src/resource.ts
+++ packages/core/src/resource.ts
@@ -1,111 +1,105 @@
-// import type { Derived } from './derived.js';
-// import { createEffect } from './effect.js';
-// import { createSignal, Signal } from './signal.js';
-// import { markUpdate, markDependency, DIRTY } from './state.js';
-// import type { ReactiveValue } from './types.js';
+import { createEffect } from './effect.js';
+import { createSignal, Signal } from './signal.js';
+import type { ReactiveValue } from './types.js';
+import { untrack } from './untrack.js';
 
-// type Fetcher<ValueType> = (source: true) => Promise<ValueType>;
-// type FetcherWithSource<SourceType, ValueType> = (source: SourceType) => Promise<ValueType>;
+type Fetcher<ValueType> = (source: true) => Promise<ValueType>;
+type FetcherWithSource<SourceType, ValueType> = (source: SourceType) => Promise<ValueType>;
 
-// export class Resource<ValueType> {
-//   private fetcher: Fetcher<ValueType>;
-//   loading = createSignal(false);
-//   error: Signal<unknown> = createSignal();
-//   sources: Array<Signal<unknown> | Derived<unknown>> | null = null;
+export class Resource<ValueType> {
+  private fetcher: Fetcher<ValueType>;
+  loading = createSignal(false);
+  error: Signal<unknown> = createSignal();
 
-//   last?: ValueType | undefined;
+  last?: ValueType | undefined;
+  current = createSignal<ValueType | undefined>(undefined);
 
-//   current?: ValueType | undefined;
+  constructor(fetcher: Fetcher<ValueType>) {
+    this.fetcher = fetcher;
+    this.fetch();
+  }
 
-//   constructor(fetcher: Fetcher<ValueType>) {
-//     this.fetcher = fetcher;
-//     this.fetch();
-//   }
+  private async fetch() {
+    this.loading.value = true;
 
-//   private async fetch() {
-//     this.loading.value = true;
+    try {
+      // We always want to go ahead and update the previous state, so that we always end up
+      // with both the previous result (if any) *and* the new value *or* error, and never
+      // end up just throwing away data.
+      this.last = untrack(() => this.current.value);
+      this.current.value = await this.fetcher(true);
+    } catch (err: unknown) {
+      this.error.value = err;
+    } finally {
+      this.loading.value = false;
+    }
+  }
 
-//     try {
-//       // We always want to go ahead and update the previous state, so that we always end up
-//       // with both the previous result (if any) *and* the new value *or* error, and never
-//       // end up just throwing away data.
-//       this.last = this.current;
-//       this.current = await this.fetcher(true);
-//       markUpdate(this, DIRTY);
-//     } catch (err: unknown) {
-//       this.error.value = err;
-//     } finally {
-//       this.loading.value = false;
-//     }
-//   }
+  get value() {
+    return this.current.value;
+  }
+}
 
-//   get value() {
-//     markDependency(this);
-//     return this.current;
-//   }
-// }
+export class ResourceWithSource<ValueType, SourceType> {
+  private fetcher: FetcherWithSource<SourceType, ValueType>;
+  loading = createSignal(false);
+  error: Signal<unknown> = createSignal();
 
-// export class ResourceWithSignal<ValueType, SourceType> {
-//   private fetcher: FetcherWithSource<SourceType, ValueType>;
-//   loading = createSignal(false);
-//   error: Signal<unknown> = createSignal();
-//   sources: Array<Signal<unknown> | Derived<unknown>> | null = null;
+  last?: ValueType | undefined;
+  current = createSignal<ValueType | undefined>(undefined);
 
-//   last?: ValueType | undefined;
-//   current?: ValueType | undefined;
+  constructor(
+    source: ReactiveValue<SourceType>,
+    fetcher: FetcherWithSource<SourceType, ValueType>
+  ) {
+    this.fetcher = fetcher;
+    createEffect(() => {
+      const value = source.value;
+      if (value === false || value === null || value === undefined) {
+        return;
+      }
+      this.fetch(value);
+    });
+  }
 
-//   constructor(
-//     source: ReactiveValue<SourceType>,
-//     fetcher: FetcherWithSource<SourceType, ValueType>
-//   ) {
-//     this.fetcher = fetcher;
-//     createEffect(() => {
-//       const value = source.value;
-//       if (value === false || value === null || value === undefined) {
-//         return;
-//       }
-//       this.fetch(value);
-//     });
-//   }
+  private async fetch(source: SourceType) {
+    this.loading.value = true;
 
-//   private async fetch(source: SourceType) {
-//     this.loading.value = true;
+    try {
+      // We always want to go ahead and update the previous state, so that we always end up
+      // with both the previous result (if any) *and* the new value *or* error, and never
+      // end up just throwing away data.
+      // We suspend tracking here so that we don't add the resource's value as a dependency to the
+      // effect that calls `fetch`
+      this.last = untrack(() => this.current.value);
+      this.current.value = await this.fetcher(source);
+    } catch (err) {
+      this.error.value = err;
+    } finally {
+      this.loading.value = false;
+    }
+  }
 
-//     try {
-//       // We always want to go ahead and update the previous state, so that we always end up
-//       // with both the previous result (if any) *and* the new value *or* error, and never
-//       // end up just throwing away data.
-//       this.last = this.current;
-//       this.current = await this.fetcher(source);
-//       markUpdate(this, DIRTY);
-//     } catch (err) {
-//       this.error.value = err;
-//     } finally {
-//       this.loading.value = false;
-//     }
-//   }
+  get value() {
+    return this.current.value;
+  }
+}
 
-//   get value() {
-//     markDependency(this);
-//     return this.current;
-//   }
-// }
-
-// export function createResource<ValueType>(fetcher: Fetcher<ValueType>): Resource<ValueType>;
-// export function createResource<ValueType, SourceType>(
-//   source: ReactiveValue<SourceType>,
-//   fetcher: FetcherWithSource<SourceType, ValueType>
-// ): ResourceWithSignal<ValueType, SourceType>;
-// export function createResource<ValueType, SourceType>(
-//   sourceOrFetcher: ReactiveValue<SourceType> | Fetcher<ValueType>,
-//   fetcher?: FetcherWithSource<SourceType, ValueType>
-// ): ResourceWithSignal<ValueType, SourceType> | Resource<ValueType> {
-//   if (typeof sourceOrFetcher === 'function') {
-//     return new Resource<ValueType>(sourceOrFetcher);
-//   } else {
-//     if (!fetcher || !(typeof fetcher === 'function')) {
-//       throw new Error('You must pass a fetcher function when creating a Resource.');
-//     }
-//     return new ResourceWithSignal<ValueType, SourceType>(sourceOrFetcher, fetcher);
-//   }
-// }
+export function createResource<ValueType>(fetcher: Fetcher<ValueType>): Resource<ValueType>;
+export function createResource<SourceType, ValueType>(
+  source: ReactiveValue<SourceType>,
+  fetcher: FetcherWithSource<SourceType, ValueType>
+): ResourceWithSource<ValueType, SourceType>;
+export function createResource<SourceType, ValueType>(
+  sourceOrFetcher: ReactiveValue<SourceType> | Fetcher<ValueType>,
+  fetcher?: FetcherWithSource<SourceType, ValueType>
+): ResourceWithSource<ValueType, SourceType> | Resource<ValueType> {
+  if (typeof sourceOrFetcher === 'function') {
+    return new Resource<ValueType>(sourceOrFetcher);
+  } else {
+    if (!fetcher || !(typeof fetcher === 'function')) {
+      throw new Error('You must pass a fetcher function when creating a Resource.');
+    }
+    return new ResourceWithSource<ValueType, SourceType>(sourceOrFetcher, fetcher);
+  }
+}
