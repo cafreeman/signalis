382fa97e8a5060c616dc2c5a4a06bc9dd2733e38
Chris Freeman <cfreeman.code@gmail.com>
2023-01-18 12:07:36 -0700

    fix spliceWhenKnown bug
    if spliceWhenKnown didn’t find any matches, it would previously set the target at the `-1` index and then return the array, which could results in a 0 length array….with 1 element at the -1 index, which is very bad

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -9,7 +9,7 @@ export function unlinkObservers(target: Derived<unknown> | Reaction) {
   }
   for (let i = 0; i < sources.length; i++) {
     const source = sources[i] as Signal<unknown> | Derived<unknown>;
-    if (!source._observers) {
+    if (!source._observers || source._observers.length === 0) {
       return;
     }
     spliceWhenKnown(source._observers, target);
@@ -19,6 +19,13 @@ export function unlinkObservers(target: Derived<unknown> | Reaction) {
 function spliceWhenKnown<T>(array: Array<T>, target: T): Array<T> {
   const idx = array.findIndex((v) => v === target);
   assert(idx !== -1, 'item must be in the array');
+  // Since `assert` gets stripped in prod builds, we want to still handle the case where we don't
+  // find a match. If we don't return early here, we end up setting the target on the index `-1`
+  // and then calling `pop`, which leaves us with an array of length 0...with a `-1` property
+  // containing the observer :sob:
+  if (idx === -1) {
+    return array;
+  }
 
   const last = array[array.length - 1];
   assert(!!last); // array cannot be empty *and* idx not be set!
