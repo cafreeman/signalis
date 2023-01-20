84a0cadd037857ab4461f750f4cb689cd3190d69
Chris Freeman <cfreeman.code@gmail.com>
2023-01-20 11:35:24 -0700

    update data-eden example

--- packages/react-dummy-app/src/fetch.ts
+++ packages/react-dummy-app/src/fetch.ts
@@ -2,14 +2,6 @@ import { buildCache } from '@data-eden/cache';
 import { buildFetch } from '@data-eden/network';
 import { createSignal } from '@signalis/core';
 
-async function loggerMiddleware(
-  request: Request,
-  next: (request: Request) => Promise<Response>
-): Promise<Response> {
-  console.log('request happening!');
-  return next(request);
-}
-
 function getUrl(input: RequestInfo | URL): string {
   if (typeof input === 'string') {
     return input;
@@ -22,63 +14,61 @@ function getUrl(input: RequestInfo | URL): string {
   return input.toString();
 }
 
-const SIGNAL = Symbol('data-eden-signal');
-
 function createHandler(): ProxyHandler<any> {
   return {
     get(target, prop, receiver) {
-      const result = Reflect.get(target, prop, receiver);
-      if (prop !== SIGNAL) {
-        target[SIGNAL].value;
-      }
-
+      const value = target.value;
+      const result = Reflect.get(value, prop, receiver);
       return result;
     },
-    set(target, prop, value) {
-      const result = Reflect.set(target, prop, value);
-      if (prop !== SIGNAL) {
-        const s = target[SIGNAL];
-        s.value = s.value + 1;
-      }
 
+    set(target, prop, value) {
+      const innerValue = target.value;
+      const result = Reflect.set(innerValue, prop, value);
+      // shallow clone to trigger signal update based on reference equality. we can improve this in the future
+      target.value = innerValue;
       return result;
     },
   };
 }
 
 export function buildCachedFetch() {
-  const fetch = buildFetch([loggerMiddleware]);
-  const cache = buildCache();
-
   const SignalCache = new Map<string, any>();
+
+  const fetch = buildFetch([]);
+
+  const cache = buildCache({
+    hooks: {
+      async commit(tx) {
+        for await (const entry of tx.entries()) {
+          const [key, entity] = entry;
+          let withSignal = SignalCache.get(key);
+
+          // Entity can also be string | number so we need to make sure it's actually an object here
+          if (entity !== null && typeof entity === 'object') {
+            if (withSignal === undefined) {
+              withSignal = new Proxy(createSignal(entity, false), handler);
+
+              SignalCache.set(key, withSignal);
+            } else {
+              Object.assign(withSignal, entity);
+            }
+          }
+        }
+      },
+    },
+  });
+
   const handler = createHandler();
 
   return async function (input: RequestInfo | URL, init?: RequestInit | undefined) {
     const key = getUrl(input);
-
     const res = await fetch(input, init).then((res) => res.json());
 
     const tx = await cache.beginTransaction();
     tx.set(key, res);
     await tx.commit();
 
-    const cacheResult = await cache.get(key);
-
-    let withSignal = SignalCache.get(key);
-
-    if (withSignal === undefined) {
-      const base = {
-        ...cacheResult,
-        [SIGNAL]: createSignal(0),
-      };
-      withSignal = new Proxy(base, handler);
-      // withSignal = createSignal(cacheResult);
-      SignalCache.set(key, withSignal);
-    } else {
-      Object.assign(withSignal, cacheResult);
-      // withSignal.value = cacheResult;
-    }
-
-    return withSignal;
+    return SignalCache.get(key);
   };
 }
